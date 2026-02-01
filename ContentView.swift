79d95b7c04d9cd47df1dea380e0f5ad4ae07c1e4
ali-Ali20201 <lyhajmry48@gmail.com>
2026-02-01 17:34:10 +0300

    ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -4,14 +4,12 @@ import WebKit
 struct WebView: UIViewRepresentable {
     let url: URL
     func makeUIView(context: Context) -> WKWebView { return WKWebView() }
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        uiView.load(URLRequest(url: url))
-    }
+    func updateUIView(_ uiView: WKWebView, context: Context) { uiView.load(URLRequest(url: url)) }
 }
 
 struct ContentView: View {
     var body: some View {
-        WebView(url: URL(string: "https://reliable-kitten-129242.netlify.app/")!)
+        WebView(url: URL(string: "https://reliable-kitten-129242.netlify.app")!)
             .edgesIgnoringSafeArea(.all)
     }
 }
